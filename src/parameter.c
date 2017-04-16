fd5c7e75ce5c674a7f516a4de3d506984cef6536
Yusuke Nomura <nomura@hemulen.ad12.riken.jp>
2017-04-16 12:51:35 +0900

    changed initial parameter

--- src/parameter.c
+++ src/parameter.c
@@ -46,7 +46,7 @@ void InitParameter() {
   for(i=0;i<NProj;i++) Proj[i] = 0.0;
   /* added by YN */ 
   for(i=0;i<NHiddenMagField;i++) HiddenMagField[i] = 0.1*(genrand_real2()-0.5);     /* TBC */
-  for(i=0;i<NHiddenPhysInt ;i++) HiddenPhysInt [i] = 0.0001*(genrand_real2()-0.5);  /* TBC */
+  for(i=0;i<NHiddenPhysInt ;i++) HiddenPhysInt [i] = 0.01*(genrand_real2()-0.5);  /* TBC */
   file1 = fopen("check_OptFlag.txt","w"); // delete
   for(i=0;i<2*NPara;i++) fprintf(file1,"%d \n", OptFlag[i]); // delete
   fclose(file1); // delete
